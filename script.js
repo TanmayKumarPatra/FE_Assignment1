191f017dc5aebd7afa638bc6d601f0fb402216fe
TanmayKumarPatra <47804936+TanmayKumarPatra@users.noreply.github.com>
2019-04-09 16:13:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,13 +35,48 @@ function volumeDecrease(){
     video.volume =0;
 }
 function reloadVideo(){
+var video = document.getElementById("video");
+var playButton = document.getElementById("play");  
+var pauseButton = document.getElementById("pause");
+var muteButton = document.getElementById("muteVideo")
     video.load();
     video.play();
+    pauseButton.disabled = false;
+    playButton.disabled = true;
 }
 function mutedVideo(){
+
    if(video.muted == false){
         video.muted = true;
+ 	
       } else{
           video.muted = false;
+
       } 
+}
+function likeVideo(){
+
+ if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("likebutton").innerHTML = localStorage.clickcount;
+  }
+
+
+}
+function unlikeVideo(){
+
+ if (typeof(Storage) !== "undefined") {
+    if (localStorage.reducecount) {
+      localStorage.reducecount = Number(localStorage.reducecount)+1;
+    } else {
+      localStorage.reducecount = 1;
+    }
+    document.getElementById("unlikebutton").innerHTML = localStorage.reducecount;
+  }
+
+
 }
